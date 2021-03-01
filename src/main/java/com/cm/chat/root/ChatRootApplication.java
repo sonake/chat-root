abb4fc9cabf44899ab5f34b38d62e983386654ab
yuanxingzhi <yuanxingzhi@moqipobing.com>
2021-03-01 20:53:30 +0800

    chat init

---
+++ src/main/java/com/cm/chat/root/ChatRootApplication.java
@@ -0,0 +1,13 @@
+package com.cm.chat.root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatRootApplication.class, args);
+    }
+
+}
