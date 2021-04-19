b7fd76b8ef5d2007e1a225268bd62b9530e392d2
yuanxingzhi <yuanxingzhi@chainup.com>
2021-04-19 20:29:56 +0800

    feat: 初始化

---
+++ src/main/java/com/cm/chat/websocket/WSServer.java
@@ -0,0 +1,38 @@
+package com.cm.chat.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @ClassName WSServer
+ * @Description <p>TODO<p>
+ * @Author DK
+ * @Date 2021/4/19 10:14
+ * @Version 1.0
+ **/
+public class WSServer {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup mainGroup = new NioEventLoopGroup();
+        EventLoopGroup subGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap
+                    .group(mainGroup, subGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new WSServerInitialzer());
+            ChannelFuture future = serverBootstrap.bind(8088).sync();
+            future.channel().closeFuture().sync();
+        }finally {
+            mainGroup.shutdownGracefully();
+            subGroup.shutdownGracefully();
+        }
+
+    }
+
+
+}
